ae7ade148362680428cd03ebf64ae808b58c324c
Milad Esfandiyari <miladisjobs@gmail.com>
2021-06-20 17:02:16 +0430

    add room database

--- app/src/main/java/com/iust/Thorium/data/entity/CellInfoDao.kt
+++ app/src/main/java/com/iust/Thorium/data/entity/CellInfoDao.kt
@@ -4,7 +4,7 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
-import com.iust.Thorium.data.model.CellInfo
+import com.iust.thorium.data.model.CellInfo
 
 @Dao
 interface CellInfoDao {
