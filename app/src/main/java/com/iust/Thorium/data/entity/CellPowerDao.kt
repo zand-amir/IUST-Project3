128a6f239068b07f944d98f0f5c201675487a9f0
Milad Esfandiyari <miladisjobs@gmail.com>
2021-06-20 16:39:44 +0430

    correct some issue for build

--- app/src/main/java/com/iust/Thorium/data/entity/CellPowerDao.kt
+++ app/src/main/java/com/iust/Thorium/data/entity/CellPowerDao.kt
@@ -14,10 +14,6 @@ interface CellPowerDao {
     @Query("SELECT * FROM CellPower WHERE id IN (:infoIds)")
     fun loadAllByIds(infoIds: IntArray): List<CellPower>
 
-//    @Query("SELECT * FROM user WHERE first_name LIKE :first AND " +
-//            "last_name LIKE :last LIMIT 1")
-//    fun findByName(first: String, last: String): CellPower
-
     @Insert
     fun insert(vararg info: CellPower)
 
