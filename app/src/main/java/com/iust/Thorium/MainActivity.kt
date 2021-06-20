8930cac7001e78bda1d34a2a036913d0c660b7c8
Milad Esfandiyari <miladisjobs@gmail.com>
2021-06-20 18:35:19 +0430

    add permission granter function to main activity

--- app/src/main/java/com/iust/Thorium/MainActivity.kt
+++ app/src/main/java/com/iust/Thorium/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.iust.Thorium
+package com.iust.thorium
 
 import android.Manifest
 import android.annotation.SuppressLint
@@ -8,37 +8,115 @@ import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
 import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import android.net.NetworkInfo
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.telephony.*
 import android.util.Log
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.*
-import com.iust.Thorium.data.AppDatabase
-import com.iust.Thorium.data.model.CellPower
+import com.iust.thorium.data.AppDatabase
+import com.iust.thorium.data.model.CellInformation
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
-import java.io.IOException
-import java.util.concurrent.TimeUnit
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
+    private var permit_s =1
+    var repeat : Int = 0
+    lateinit var tm : TelephonyManager
+    private var db: AppDatabase? = null
+    var handler: Handler = Handler()
+    val delayer = 700
+    lateinit var mFusedLocationClient: FusedLocationProviderClient
+    var latitude : Double = 35.0
+    var longitude : Double = 40.0
+    var record_size :Int =0
+    lateinit var cm: ConnectivityManager
+    lateinit var spinner: Spinner
+    var list_position: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
     }
 
 
+    private fun isLocationEnabled(): Boolean {
+        var locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation() {
+        if (isLocationEnabled()) {
+
+            mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+                var location: Location? = task.result
+                requestNewLocationData()
+            }
+        } else {
+            Toast.makeText(this, "Turn on location", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestNewLocationData() {
+        var mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 1500
+        mLocationRequest.fastestInterval = 500
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        mFusedLocationClient!!.requestLocationUpdates(
+            mLocationRequest, mLocationCallback,
+            Looper.myLooper()
+        )
+    }
+
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            var mLastLocation: Location = locationResult.lastLocation
+            latitude = mLastLocation.latitude
+            longitude= mLastLocation.longitude
+        }
+    }
+
+    private fun permissiongranter() {
+        Dexter.withContext(this)
+            .withPermissions(
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(p0: MultiplePermissionsReport?) {
+                    Toast.makeText(this@MainActivity, "permission granted", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    p0: MutableList<com.karumi.dexter.listener.PermissionRequest>?,
+                    p1: PermissionToken?
+                ) {
+                    ActivityCompat.requestPermissions(
+                        this@MainActivity, arrayOf(
+                            Manifest.permission.READ_PHONE_STATE,
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        ), permit_s
+                    )
+
+
+                }
+
+            }).check()
+    }
 }
